0037388decf9ca8a8863241c18d35bc5d6b2cbc0
sofiakonovalova <sofia@orangemason.com>
2021-04-05 16:22:20 +0200

    added some comments, outRight easyL

--- src/rebalancing.lean
+++ src/rebalancing.lean
@@ -20,15 +20,13 @@ def balanced {α : Type} : btree α → bool
 | btree.empty := tt
 | (btree.node l k a r) := (height l - height r) ≤ 1
 
-/- # A left-heavy tree -/ 
---        3
---       /
---      2
---     /
---    1
-
 def leftHeavy : btree string := btree.node (btree.node (btree.node btree.empty 1 "c" btree.empty) 2 "b" btree.empty) 3 "a" btree.empty
 
+def rightHeavy : btree string := btree.node btree.empty 1 "a" (btree.node btree.empty 2 "b" (btree.node btree.empty 3 "c" btree.empty))
+
+/-- 
+  Definition of a tree being outside left-heavy
+-/
 def outLeft {α : Type} : btree α → bool
 | btree.empty := ff
 | (btree.node (btree.node xL x a xR) z d zR) := 
@@ -36,10 +34,32 @@ def outLeft {α : Type} : btree α → bool
   (height xR ≥ height zR) ∧ height (xL) = height (zR) + 1
 | (btree.node l k a r) := ff
 
+/-- 
+  Definition of a tree being outside right-heavy
+-/
+def outRight {α : Type} : btree α → bool
+| btree.empty := ff
+| (btree.node zL z d (btree.node yL y b yR)) :=
+  (height yL ≤ height yR) ∧ (height yL ≤ height yR + 1) ∧
+  (height yR ≥ height zL) ∧ (height zL + 1 = height yR)
+| (btree.node l k a r) := ff
+
+/--
+  Simple right rotation
+-/
 def easyR {α : Type} : btree α → btree α
 | btree.empty := btree.empty
 | (btree.node (btree.node xL x a xR) z d zR) := 
   (btree.node xL x a (btree.node xR z d zR))
+| (btree.node l k a r) := btree.node l k a r
+
+/--
+  Simple left rotation
+-/
+def easyL {α : Type} : btree α → btree α
+| btree.empty := btree.empty
+| (btree.node zL z d (btree.node yL y b yR)) :=
+  (btree.node (btree.node zL z d yL) y b yR)
 | (btree.node l k a r) := btree.node l k a r
 
 end rebalancing
