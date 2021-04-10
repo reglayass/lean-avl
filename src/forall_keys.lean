7ff4301a8a8419d6f9031b7fd198d0f5da8d1ad3
sofiakonovalova <sofia@orangemason.com>
2021-04-10 15:39:12 +0200

    started forall_insert lemma

--- src/forall_keys.lean
+++ src/forall_keys.lean
@@ -5,10 +5,44 @@ set_option pp.generalized_field_notation false
 namespace forall_keys_lemmas
 open btree_def
 
-lemma forall_insert {α : Type} (tk k : nat) (tl : btree α) (a : α) (p : nat → nat → Prop) (h : p k tk) :
-  forall_keys p tk tl → forall_keys p tk (insert k a tl) :=
+lemma forall_insert {α : Type} (k' k : nat) (t : btree α) (a : α) (p : nat → nat → Prop) (h : p k k') :
+  forall_keys p k' t → forall_keys p k' (insert k a t) :=
 begin
-  sorry 
+  intro h₁,
+  induction t,
+  case empty {
+    sorry
+  },
+  case node : tl tk ta tr ihl ihr {
+    simp only [btree_def.insert],
+    simp [forall_keys] at h₁, 
+    by_cases c₁ : (k < tk),
+    { simp only [if_pos c₁, forall_keys], 
+      apply and.intro,
+      { sorry },
+      {
+        apply and.intro 
+          (and.elim_left (and.elim_right h₁)) 
+          (and.elim_right (and.elim_right h₁))
+      }
+    },
+    { simp only [if_neg c₁, forall_keys], 
+      by_cases c₂ : (k > tk),
+      { simp only [if_pos c₂, forall_keys], 
+        apply and.intro,
+        { apply and.elim_left h₁ },
+        { apply and.intro,
+          { apply and.elim_left (and.elim_right h₁) },
+          { sorry }
+        }
+      },
+      { simp only [if_neg c₂, forall_keys], 
+        apply and.intro,
+        { sorry },
+        { sorry }
+      }
+    }
+  }
 end
 
 end forall_keys_lemmas
