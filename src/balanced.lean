3d6306448f14cbf6041f9ea92ef0dcf2f98cf690
sofiakonovalova <sofia@orangemason.com>
2021-04-10 17:22:21 +0200

    began rotation lemmas

--- src/balanced.lean
+++ src/balanced.lean
@@ -22,8 +22,6 @@ def balanced {α : Type} : btree α → bool
 
 def leftHeavy : btree string := btree.node (btree.node (btree.node btree.empty 1 "c" btree.empty) 2 "b" btree.empty) 3 "a" btree.empty
 
--- def rightHeavy : btree string := btree.node btree.empty 1 "a" (btree.node btree.empty 2 "b" (btree.node btree.empty 3 "c" btree.empty))
-
 /-- 
   Definition of a tree being outside left-heavy
 -/
@@ -31,18 +29,8 @@ def outLeft {α : Type} : btree α → bool
 | btree.empty := ff
 | (btree.node (btree.node xL x a xR) z d zR) := 
   (height xL ≥ height xR) ∧ (height xL ≤ height xR + 1) ∧ 
-  (height xR ≥ height zR) ∧ height (xL) = height (zR) + 1
+  (height xR ≥ height zR) ∧ (height xL = height zR + 1)
 | (btree.node l k a r) := ff
-
--- /-- 
---   Definition of a tree being outside right-heavy
--- -/
--- def outRight {α : Type} : btree α → bool
--- | btree.empty := ff
--- | (btree.node zL z d (btree.node yL y b yR)) :=
---   (height yL ≤ height yR) ∧ (height yL ≤ height yR + 1) ∧
---   (height yR ≥ height zL) ∧ (height zL + 1 = height yR)
--- | (btree.node l k a r) := ff
 
 /--
   Simple right rotation
@@ -54,38 +42,15 @@ def easyR {α : Type} : btree α → btree α
 | (btree.node l k a r) := btree.node l k a r
 
 -- /--
---   Simple left rotation
+--   Insertion with balancing
 -- -/
--- def easyL {α : Type} : btree α → btree α
--- | btree.empty := btree.empty
--- | (btree.node zL z d (btree.node yL y b yR)) :=
---   (btree.node (btree.node zL z d yL) y b yR)
--- | (btree.node l k a r) := btree.node l k a r
-
--- def rotR {α : Type} : btree α → btree α
--- | btree.empty := btree.empty
--- | (btree.node (btree.node xL x a xR) z d zR) := 
---   if height xL < height xR then easyR (btree.node (easyL (btree.node xL x a xR)) z d zR)
---   else easyR (btree.node (btree.node xL x a xR) z d zR)
--- | (btree.node l k a r) := btree.node l k a r
-
--- def rotL {α : Type} : btree α → btree α
--- | btree.empty := btree.empty
--- | (btree.node zL z d (btree.node yL y b yR)) :=
---   if height yR < height yL then easyL (btree.node zL z d (easyR (btree.node yL y b yR)))
---   else easyL (btree.node zL z d (btree.node yL y b yR))
--- | (btree.node l k a r) := btree.node l k a r
-
-/--
-  Insertion with balancing
--/
-def insert {α : Type} (x : nat) (a : α) : btree α → btree α
-| btree.empty := btree.node btree.empty x a btree.empty
-| (btree.node l k a' r) :=
-  if x < k then 
-    if outLeft (btree.node (insert l) k a' r) then easyR (btree.node (insert l) k a' r)
-    else btree.node (insert l) k a' r
-  else if x > k then sorry
-  else btree.node l x a r
+-- def insert {α : Type} (x : nat) (a : α) : btree α → btree α
+-- | btree.empty := btree.node btree.empty x a btree.empty
+-- | (btree.node l k a' r) :=
+--   if x < k then 
+--     if outLeft (btree.node (insert l) k a' r) then easyR (btree.node (insert l) k a' r)
+--     else btree.node (insert l) k a' r
+--   else if x > k then sorry
+--   else btree.node l x a r
 
 end balanced
