d94ac4f509ace36244bb769f4832d9c6f0cfb454
sofiakonovalova <sofia@orangemason.com>
2021-04-30 10:08:10 +0300

    finished main rotation lemmas

--- src/rotations.lean
+++ src/rotations.lean
@@ -9,6 +9,76 @@ open btree
 
 variables {α : Type u}
 
+lemma forall_keys_trans (r : btree α) (p : nat → nat → Prop) (z x : nat) (h₁ : p x z) (h₂ : ∀ a b c, p a b → p b c → p a c) :
+  forall_keys p z r → forall_keys p x r :=
+begin
+  induction r,
+  case empty { simp [forall_keys], },
+  case node : rl rk ra rr ihl ihr { 
+    simp [forall_keys],
+    intros h₃ h₄ h₅,
+    split,
+    { finish, },
+    { split, 
+      { apply h₂, apply h₁, finish, },
+      { finish, },
+    },
+  },
+end
+
+lemma easyL_ordered (rl rr l : btree α) (x z : nat) (a d : α) :
+  ordered (btree.node l z d (btree.node rl x a rr)) → 
+    ordered (easyL (btree.node l z d (btree.node rl x a rr))) :=
+begin
+  intro h₁,
+  simp only [easyL, ordered],
+  simp only [ordered] at h₁,
+  apply and.intro,
+  { apply and.intro, 
+    { finish, },
+    { apply and.intro, 
+      { finish, },
+      { apply and.intro,
+        { finish, },
+        { apply and.elim h₁, 
+          intros h₂ h₃,
+          apply and.elim h₃,
+          intros h₄ h₅,
+          apply and.elim h₅,
+          intros h₆ h₇,
+          rw forall_keys at h₇,
+          finish,
+        }, 
+      },
+    },
+  },
+  { apply and.intro, 
+    { finish, },
+    { apply and.intro,
+      { rw forall_keys, 
+        apply and.elim h₁,
+        intros h₂ h₃,
+        apply and.elim h₃,
+        intros h₄ h₅,
+        apply and.elim h₅,
+        intros h₆ h₇,
+        rw forall_keys at h₇,
+        apply and.intro,
+        { apply forall_keys_trans _ (>) z _, 
+          { finish, },
+          { apply trans, },
+          { finish, },
+        },
+        { apply and.intro,
+          { finish, },
+          { finish, }, 
+        },
+      },
+      { finish, }, 
+    },
+  },
+end
+
 lemma easyR_ordered (ll lr r : btree α) (x z : nat) (a d : α) : 
   ordered (btree.node (btree.node ll x a lr) z d r) → 
     ordered (easyR (btree.node (btree.node ll x a lr) z d r)) :=
@@ -51,7 +121,11 @@ begin
           rw forall_keys at h₆,
           apply and.intro,
           { finish, },
-          { sorry }, 
+          { apply forall_keys_trans _ (<) z _, 
+            { finish, },
+            { apply has_lt.lt.trans, },
+            { finish, },
+          }, 
         },
       },
     }, 
