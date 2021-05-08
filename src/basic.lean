2066f8276ec059612f27288d84d429afee0f257b
sofiakonovalova <sofia@orangemason.com>
2021-05-08 15:48:22 +0300

    added insert_bal

--- src/basic.lean
+++ src/basic.lean
@@ -4,6 +4,7 @@ inductive btree (α : Type u)
 | empty {} : btree
 | node (l : btree) (k : nat) (a : α) (r : btree) : btree
 
+-- TODO: remove once done
 def tostring : btree string → string
 | btree.empty := "_"
 | (btree.node l k a r) := "[" ++ " " ++ to_string k ++ " " ++ tostring l ++ " " ++ tostring r ++ " " ++ "]"
@@ -55,16 +56,16 @@ def height : btree α → nat
 | (btree.node l k a r) :=
   1 + (max (height l) (height r))
 
-def bf : btree α → nat
-| btree.empty := 0
+def balanced : btree α → bool
+| btree.empty := tt
 | (btree.node l k a r) :=
-  height l - height r
+  height l - height r ≤ 1
 
 def outLeft : btree α → bool
 | btree.empty := ff
 | (btree.node l k a r) :=
   match l with
-  | btree.empty := ff
+  | btree.empty := outLeft l
   | (btree.node ll lk la lr) := 
     (height ll ≥ height lr) ∧ (height ll ≤ height lr + 1) ∧ 
     (height lr ≥ height r) ∧ (height ll = height r + 1)
@@ -74,7 +75,7 @@ def outRight : btree α → bool
 | btree.empty := ff
 | (btree.node l k a r) :=
   match r with
-  | btree.empty := ff
+  | btree.empty := outRight r
   | (btree.node rl rk ra rr) :=
     (height rl ≤ height rr) ∧ (height rl ≤ height rr + 1) ∧
     (height rr ≥ height l) ∧ (height l + 1 = height rr)
@@ -104,17 +105,28 @@ def rotR : btree α → btree α
   | (btree.node ll lk la lr) :=
     if (height ll < height lr) then easyR (btree.node (easyL l) k a r)
     else easyR (btree.node l k a r)
-  end 
+  end
 
 def rotL : btree α → btree α
 | btree.empty := btree.empty
 | (btree.node l k a r) :=
   match r with
-  | btree.empty := btree.node l k a r
+  | btree.empty := (btree.node l k a r)
   | (btree.node rl rk ra rr) :=
     if height rr < height rl then easyL (btree.node l k a (easyR r))
     else easyL (btree.node l k a r)
   end
+
+def insert_bal (x : nat) (a : α) : btree α → btree α
+| btree.empty := btree.node btree.empty x a btree.empty
+| (btree.node l k a' r) :=
+  if x < k then 
+    if (height (insert_bal l) > (height r + 1)) then rotR (btree.node (insert_bal l) k a' r) 
+    else (btree.node (insert_bal l) k a' r)
+  else if x > k then
+    if (height (insert_bal r) > (height l + 1)) then rotL (btree.node l k a' (insert_bal r))
+    else (btree.node l k a' (insert_bal r))
+  else (btree.node l x a r)
 
 end balancing
 
