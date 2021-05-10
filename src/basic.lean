39ad654923fe705f65e7769d89f0e8b207507755
sofiakonovalova <sofia@orangemason.com>
2021-05-10 13:48:32 +0300

    completed rotation lemma because i'm a dumbass

--- src/basic.lean
+++ src/basic.lean
@@ -39,15 +39,15 @@ def insert (x : nat) (a : α) : btree α → btree α
 
 section ordering
 
-def forall_keys (p : nat → nat → Prop) : nat → btree α → Prop
-| x btree.empty := tt
-| x (btree.node l k a r) := 
-  forall_keys x l ∧ (p x k) ∧ forall_keys x r
+def forall_keys (p : nat → nat → Prop) : btree α → nat → Prop
+| btree.empty x := tt
+| (btree.node l k a r) x := 
+  forall_keys l x ∧ (p x k) ∧ forall_keys r x
 
 def ordered : btree α → Prop
 | btree.empty := tt 
 | (btree.node l k a r) := 
-  ordered l ∧ ordered r ∧ (forall_keys (>) k l) ∧ (forall_keys (<) k r)
+  ordered l ∧ ordered r ∧ (forall_keys (>) l k) ∧ (forall_keys (<) r k)
 
 end ordering
 
@@ -86,37 +86,47 @@ def outRight : btree α → bool
 def easyR : btree α → btree α
 | btree.empty := btree.empty
 | (btree.node (btree.node ll lk la lr) k a r) := btree.node ll lk la (btree.node lr k a r)
-| (btree.node l k a r) := (btree.node l k a r)
+| (btree.node l k a r) := btree.node l k a r
 
 def easyL : btree α → btree α
 | btree.empty := btree.empty
 | (btree.node l k a (btree.node rl rk ra rr)) := (btree.node (btree.node l k a rl) rk ra rr)
-| (btree.node l k a r) := (btree.node l k a r)
-
-def rotR: btree α → btree α
-| btree.empty := btree.empty
-| (btree.node (btree.node ll lk la lr) k a r) :=
-  if (height ll < height lr) then easyR (btree.node (easyL (btree.node ll lk la lr)) k a r)
-  else easyR (btree.node (btree.node ll lk la lr) k a r)
 | (btree.node l k a r) := btree.node l k a r
 
-def rotL : btree α → btree α
+def rotR : btree α → btree α
 | btree.empty := btree.empty
-| (btree.node l k a (btree.node rl rk ra rr)) :=
-  if (height rr < height rl) then easyL (btree.node l k a (easyR (btree.node rl rk ra rr)))
-  else easyL (btree.node l k a (btree.node rl rk ra rr))
-| (btree.node l k a r) := btree.node l k a r
+| (btree.node l k a r) :=
+  match l with
+  | btree.empty := (btree.node l k a r)
+  | (btree.node ll lk la lr) := 
+    if height ll < height lr then easyR (btree.node (easyL l) k a r)
+    else easyR (btree.node l k a r)
+  end
 
-def insert_bal (x : nat) (a : α) : btree α → btree α
-| btree.empty := btree.node btree.empty x a btree.empty
-| (btree.node l k a' r) :=
-  if x < k then 
-    if (height (insert_bal l) > (height r + 1)) then rotR (btree.node (insert_bal l) k a' r) 
-    else (btree.node (insert_bal l) k a' r)
-  else if x > k then
-    if (height (insert_bal r) > (height l + 1)) then rotL (btree.node l k a' (insert_bal r))
-    else (btree.node l k a' (insert_bal r))
-  else (btree.node l x a r)
+-- def rotR: btree α → btree α
+-- | btree.empty := btree.empty
+-- | (btree.node (btree.node ll lk la lr) k a r) :=
+--   if (height ll < height lr) then easyR (btree.node (easyL (btree.node ll lk la lr)) k a r)
+--   else easyR (btree.node ll k a r)
+-- | (btree.node l k a r) := (btree.node l k a r)
+
+-- def rotL : btree α → btree α
+-- | btree.empty := btree.empty
+-- | (btree.node l k a (btree.node rl rk ra rr)) :=
+--   if (height rr < height rl) then easyL (btree.node l k a (easyR (btree.node rl rk ra rr)))
+--   else easyL (btree.node l k a (btree.node rl rk ra rr))
+-- | (btree.node l k a r) := btree.node l k a r
+
+-- def insert_bal (x : nat) (a : α) : btree α → btree α
+-- | btree.empty := btree.node btree.empty x a btree.empty
+-- | (btree.node l k a' r) :=
+--   if x < k then 
+--     if (height (insert_bal l) > (height r + 1)) then rotR (btree.node (insert_bal l) k a' r) 
+--     else btree.node (insert_bal l) k a' r
+--   else if x > k then
+--     if (height (insert_bal r) > (height l + 1)) then rotL (btree.node l k a' (insert_bal r))
+--     else btree.node l k a' (insert_bal r)
+--   else btree.node l x a r
 
 end balancing
 
