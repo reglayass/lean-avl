1725ee5fe497c9d6fe828d3eae3507dfd61661a6
Jannis Limperg <jannis@limperg.de>
2021-05-27 15:22:49 +0200

    Introduce views for shrink and delRoot

--- src/basic.lean
+++ src/basic.lean
@@ -1,4 +1,9 @@
+import tactic.rcases
+import tactic.linarith
+
 universe u
+
+set_option pp.generalized_field_notation false
 
 inductive btree (α : Type u)
 | empty {} : btree
@@ -11,9 +16,9 @@ def empty_tree : btree α := btree.empty
 
 def lookup (x : nat) : btree α → option α
 | btree.empty := none
-| (btree.node l k a r) := 
-  if x < k then lookup l 
-  else if x > k then lookup r 
+| (btree.node l k a r) :=
+  if x < k then lookup l
+  else if x > k then lookup r
   else a
 
 def bound (x : nat) : btree α → bool
@@ -30,12 +35,12 @@ def insert (x : nat) (a : α) : btree α → btree α
 
 def forall_keys (p : nat → nat → Prop) : btree α → nat → Prop
 | btree.empty x := tt
-| (btree.node l k a r) x := 
+| (btree.node l k a r) x :=
   forall_keys l x ∧ (p x k) ∧ forall_keys r x
 
 def ordered : btree α → Prop
-| btree.empty := tt 
-| (btree.node l k a r) := 
+| btree.empty := tt
+| (btree.node l k a r) :=
   ordered l ∧ ordered r ∧ (forall_keys (>) l k) ∧ (forall_keys (<) r k)
 
 def height : btree α → nat
@@ -54,8 +59,8 @@ def outLeft : btree α → bool
 | (btree.node l k a r) :=
   match l with
   | btree.empty := ff
-  | (btree.node ll lk la lr) := 
-    (height ll ≥ height lr) ∧ (height ll ≤ height lr + 1) ∧ 
+  | (btree.node ll lk la lr) :=
+    (height ll ≥ height lr) ∧ (height ll ≤ height lr + 1) ∧
     (height lr ≥ height r) ∧ (height r + 1 = height ll)
   end
 
@@ -65,7 +70,7 @@ def outRight : btree α → bool
   match r with
   | btree.empty := ff
   | (btree.node rl rk ra rr) :=
-    (height rr ≥ height rl) ∧ (height rr ≤ height rl + 1) ∧ 
+    (height rr ≥ height rl) ∧ (height rr ≤ height rl + 1) ∧
     (height rl ≤ height l) ∧ (height l + 1 = height rr)
   end
 
@@ -84,7 +89,7 @@ def rotR : btree α → btree α
 | (btree.node l k a r) :=
   match l with
   | btree.empty := (btree.node l k a r)
-  | (btree.node ll lk la lr) := 
+  | (btree.node ll lk la lr) :=
     if height ll < height lr then easyR (btree.node (easyL l) k a r)
     else easyR (btree.node l k a r)
   end
@@ -92,7 +97,7 @@ def rotR : btree α → btree α
 def rotL : btree α → btree α
 | btree.empty := btree.empty
 | (btree.node l k a r) :=
-  match r with 
+  match r with
   | btree.empty := (btree.node l k a r)
   | (btree.node rl rk ra rr) :=
     if height rr < height rl then easyL (btree.node l k a (easyR r))
@@ -111,41 +116,104 @@ def insert_balanced (x : nat) (v : α) : btree α → btree α
 | (btree.node l k a r) :=
   let nl := insert_balanced l in
   let nr := insert_balanced r in
-  if x < k then 
+  if x < k then
     if height nl > height r + 1 then rotR (btree.node nl k a r)
     else btree.node nl k a r
-  else if x > k then 
+  else if x > k then
     if height nr > height l + 1 then rotL (btree.node l k a nr)
     else btree.node l k a nr
   else btree.node l x v r
 
-def shrink_core : option (nat × α × btree α) → btree α → nat → α → btree α → (nat × α × btree α)
-| none l k v r := (k, v, l)
-| (some (x, a, sh)) l k v r := 
-  if height l > height sh + 1 then (x, a, rotR (btree.node sh k v l))
-  else (x, a, (btree.node sh k v l))
-
 def shrink : btree α → option (nat × α × btree α)
 | btree.empty := none
-| (btree.node l k v r) := some (shrink_core (shrink r) l k v r)
+| (btree.node l k v r) := some $
+  match shrink r with
+  | none := (k, v, l)
+  | some (x, a, sh) :=
+    if height l > height sh + 1
+      then (x, a, rotR (btree.node sh k v l))
+      else (x, a, btree.node sh k v l)
+  end
 
-def delRoot_core : option (nat × α × btree α) → btree α → nat → α → btree α → btree α
-| none l k v r := r
-| (some (x, a, sh)) l k v r :=
-  if height r > height sh + 1 then rotL (btree.node sh x a r)
-  else btree.node sh x a r
+inductive shrink_view {α} : btree α → option (nat × α × btree α) → Sort*
+| empty : shrink_view empty none
+| nonempty_empty : ∀ {l k v r},
+  shrink r = none →
+  shrink_view (node l k v r) (some (k, v, l))
+| nonempty_nonempty₁ : ∀ {l k v r x a sh out},
+  shrink r = some (x, a, sh) →
+  height l > height sh + 1 →
+  out = some (x, a, rotR (btree.node sh k v l)) →
+  shrink_view (node l k v r) out
+| nonempty_nonempty₂ : ∀ {l k v r x a sh},
+  shrink r = some (x, a, sh) →
+  height l ≤ height sh + 1 →
+  shrink_view (node l k v r) (some (x, a, node sh k v l))
+
+lemma shrink_shrink_view (t : btree α) : shrink_view t (shrink t) :=
+begin
+ sorry
+end
 
 def delRoot : btree α → btree α
 | btree.empty := btree.empty
-| (btree.node l k v r) := delRoot_core (shrink l) l k v r
+| (btree.node l k v r) :=
+  match shrink l with
+  | none := r
+  | some (x, a, sh) :=
+    if height r > height sh + 1
+      then rotL (node sh x a r)
+      else node sh x a r
+  end
+
+/- delRoot_view t dt ↔ delRoot t = dt -/
+inductive delRoot_view {α} : btree α → btree α → Sort*
+| empty : delRoot_view empty empty
+| nonempty_empty : ∀ {l k v r},
+  shrink l = none →
+  delRoot_view (node l k v r) r
+| nonempty_nonempty₁ : ∀ {l k v r x a sh},
+  shrink l = some (x, a, sh) →
+  height r > height sh + 1 →
+  delRoot_view (node l k v r) (rotL (node sh x a r))
+| nonempty_nonempty₂ : ∀ {l k v r x a sh},
+  shrink l = some (x, a, sh) →
+  height r ≤ height sh + 1 →
+  delRoot_view (node l k v r) (node sh x a r)
+
+lemma delRoot_delRoot_view (t : btree α) : delRoot_view t (delRoot t) :=
+begin
+  cases t,
+  case empty {
+    exact delRoot_view.empty,
+  },
+  case node : l k a r {
+    dsimp [delRoot],
+    cases h : shrink l,
+    case none {
+      dsimp only [delRoot._match_1],
+      apply delRoot_view.nonempty_empty ; assumption,
+    },
+    case some {
+      rcases val with ⟨x, a', sh⟩,
+      dsimp only [delRoot._match_1],
+      by_cases h' : height r > height sh + 1,
+      { simp only [if_pos h'],
+        apply delRoot_view.nonempty_nonempty₁ ; assumption },
+      { simp only [if_neg h'],
+        apply delRoot_view.nonempty_nonempty₂ ; try { assumption },
+        linarith }
+    }
+  }
+end
 
 def delete (x : nat) : btree α → btree α
 | btree.empty := btree.empty
-| (btree.node l k a r) := 
+| (btree.node l k a r) :=
   let dl := delete l in
   let dr := delete r in
   if x = k then delRoot (btree.node l k a r)
-  else if x < k then 
+  else if x < k then
     if height r > height dl + 1 then rotL (btree.node dl k a r)
     else btree.node dl k a r
   else if height l > height dr + 1 then rotR (btree.node l k a dr)
--- src/deletion.lean
+++ src/deletion.lean
@@ -1,6 +1,7 @@
 import basic
 import rotations
 import tactic.linarith
+import tactic.induction
 set_option pp.generalized_field_notation false
 
 universe u
@@ -17,16 +18,6 @@ variables {α : Type u}
 --   simp [delRoot, shrink, delRoot_core],
 -- end
 
-lemma delRoot_some (l r sh : btree α) (x k : nat) (v a : α) (h : shrink l = some (x, a, sh)) :
-  delRoot (btree.node l k v r) = 
-    if height r > height sh + 1 then rotL (btree.node sh x a r) 
-    else btree.node sh x a r :=
-begin
-  simp [delRoot], 
-  rw h,
-  refl,
-end
-
 lemma delRoot_ordered (t : btree α) :
   ordered t → ordered (delRoot t) :=
 begin
@@ -36,21 +27,16 @@ begin
     simp [delRoot, ordered],
   },
   case node: tl tk ta tr ihl ihr {
-    cases tl,
-    case empty {
-      simp [delRoot, shrink, delRoot_core],
-      simp [ordered] at h₁,
-      apply and.left h₁,
+    cases' delRoot_delRoot_view (node tl tk ta tr),
+    case nonempty_empty {
+      sorry
     },
-    case node : tll tlk tla tlr {
-      simp [delRoot, shrink, delRoot_core],
-      rcases (shrink_core (shrink tlr) tll tlk tla tlr) with ⟨x, a, sh⟩,
-      simp [delRoot_core],
-      by_cases c₁ : (height sh + 1 < height tr),
-      { simp [if_pos c₁],
-        sorry,
-      },
-      { sorry },
+    case nonempty_nonempty₁ {
+      rw h_2; clear h_2,
+      sorry,
+    },
+    case nonempty_nonempty₂ {
+      sorry
     }
   }
 end
@@ -68,14 +54,14 @@ begin
     simp [delete],
     simp [ordered] at h₁,
     by_cases c₁ : (k = tk),
-    { simp [if_pos c₁], 
+    { simp [if_pos c₁],
       apply delRoot_ordered,
       rw ordered,
       assumption,
     },
     { simp [if_neg c₁],
       by_cases c₂ : (k < tk),
-      { simp [if_pos c₂], 
+      { simp [if_pos c₂],
         by_cases c₃ : (height (delete k tl) + 1 < height tr),
         { simp only [if_pos c₃],
           sorry,
@@ -96,19 +82,19 @@ begin
       },
       { simp [if_neg c₂],
         by_cases c₃ : (height (delete k tr) + 1 < height tl),
-        { simp [if_pos c₃], 
+        { simp [if_pos c₃],
           sorry,
         },
-        { simp [if_neg c₃, ordered], 
+        { simp [if_neg c₃, ordered],
           split,
           { apply and.left h₁, },
           { split,
             { apply ihr,
-              apply and.left (and.right h₁), 
+              apply and.left (and.right h₁),
             },
             { split,
               { apply and.left (and.right (and.right h₁)), },
-              { sorry, }, 
+              { sorry, },
             },
           },
         },
