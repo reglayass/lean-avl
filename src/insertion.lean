8d83093b60e1b51fd61c646cabd8781a4873d776
sofiakonovalova <sofia@orangemason.com>
2021-07-06 20:48:01 +0200

    refactored for insert

--- src/insertion.lean
+++ src/insertion.lean
@@ -14,45 +14,26 @@ open forall_keys_lemmas
 
 variables {α : Type u}
 
-lemma forall_insert_balanced (k x : nat) (t : btree α) (a : α) (p : nat → nat → Prop) (h : p x k) :
-  forall_keys p x t → forall_keys p x (insert_balanced k a t) :=
+lemma forall_insert (k x : nat) (t : btree α) (a : α) (p : nat → nat → Prop) (h : p x k) :
+  forall_keys p x t → forall_keys p x (insert k a t) :=
 begin
   intro h₁,
   induction t,
   case empty {
     unfold forall_keys,
     intros k' h₂,
-    simp [insert_balanced, bound] at h₂,
+    simp [btree.insert, bound] at h₂,
     subst h₂,
     exact h,
   },
   case node : tl tk ta tr ihl ihr {
     unfold forall_keys at ihl ihr h₁,
-    simp only [insert_balanced],
+    simp only [btree.insert],
     by_cases c₁ : (k < tk),
     { simp only [if_pos c₁], 
-      by_cases c₂ : (height (insert_balanced k a tl) > height tr + 1),
+      by_cases c₂: ↥(left_heavy (insert k a tl)),
       { simp only [if_pos c₂], 
         apply forall_rotate_right,
-        apply forall_keys_intro,
-        unfold forall_keys,
-        repeat { split },
-        { apply ihl,
-          intros k' h₂,
-          apply h₁,
-          simp [bound],
-          tauto,
-        },
-        { apply h₁, 
-          simp [bound],
-        },
-        { intros k' h₂, 
-          apply h₁,
-          simp [bound],
-          tauto,
-        },
-      },
-      { simp only [if_neg c₂], 
         apply forall_keys_intro,
         unfold forall_keys,
         repeat { split },
@@ -71,23 +52,42 @@ begin
           tauto,
         },
       },
+      { simp only [if_neg c₂],
+        apply forall_keys_intro,
+        unfold forall_keys,
+        repeat { split },
+        { apply ihl, 
+          intros k' h₂,
+          apply h₁,
+          simp [bound],
+          tauto,
+        },
+        { apply h₁, 
+          simp [bound],
+        },
+        { intros k' h₂, 
+          apply h₁,
+          simp [bound],
+          tauto,
+        }, 
+      },
     },
     { simp only [if_neg c₁], 
       by_cases c₂ : (k > tk),
       { simp only [if_pos c₂], 
-        by_cases c₃ : (height (insert_balanced k a tr) > height tl + 1),
+        by_cases c₃ : ↥(right_heavy (insert k a tr)),
         { simp only [if_pos c₃], 
           apply forall_rotate_left,
           apply forall_keys_intro,
           unfold forall_keys,
           repeat { split },
-          { intros k' h₂, 
+          { intros k' h₂,
             apply h₁,
             simp [bound],
-            tauto,
+            tauto, 
           },
-          { apply h₁, 
-            simp [bound],
+          { apply h₁,
+            simp [bound], 
           },
           { apply ihr, 
             intros k' h₂,
@@ -108,7 +108,7 @@ begin
           { apply h₁, 
             simp [bound],
           },
-          { apply ihr,
+          { apply ihr, 
             intros k' h₂,
             apply h₁,
             simp [bound],
@@ -118,23 +118,20 @@ begin
       },
       { simp only [if_neg c₂], 
         have h : k = tk := by linarith,
-        unfold forall_keys,
-        intros k' h₂,
-        apply h₁,
-        rw h at h₂,
-        exact h₂,
+        subst h,
+        exact h₁,
       },
     },
   },
 end
 
 lemma insert_ordered (t : btree α) (k : nat) (v : α) :
-  ordered t → ordered (insert_balanced k v t) :=
+  ordered t → ordered (insert k v t) :=
 begin
   intro h₁,
   induction t,
   case empty {
-    simp [insert_balanced, ordered, forall_keys],
+    simp [btree.insert, ordered, forall_keys],
     split,
     { intros k' h₂, 
       simp [bound] at h₂,
@@ -146,17 +143,17 @@ begin
     },
   },
   case node : tl tk ta tr ihl ihr {
-    simp only [insert_balanced],
+    simp only [btree.insert],
     by_cases c₁ : (k < tk),
     { simp only [if_pos c₁], 
-      by_cases c₂ : (height (insert_balanced k v tl) > height tr + 1),
+      by_cases c₂ : ↥(left_heavy (insert k v tl)),
       { simp only [if_pos c₂], 
         apply rotate_right_ordered,
         rw ordered at h₁ ⊢,
         cases_matching* (_ ∧ _),
         repeat { split }; try { assumption },
         { apply ihl, assumption, },
-        { apply forall_insert_balanced,
+        { apply forall_insert,
           { exact c₁, },
           { assumption },
         },
@@ -166,7 +163,7 @@ begin
         cases_matching* (_ ∧ _),
         repeat { split }; try { assumption },
         { apply ihl, assumption, },
-        { apply forall_insert_balanced, 
+        { apply forall_insert, 
           { exact c₁, },
           { assumption, },
         }
@@ -175,14 +172,14 @@ begin
     { simp only [if_neg c₁], 
       by_cases c₂ : (k > tk),
       { simp only [if_pos c₂], 
-        by_cases c₃ : (height (insert_balanced k v tr) > height tl + 1),
+        by_cases c₃ : ↥(right_heavy (insert k v tr)),
         { simp only [if_pos c₃], 
           apply rotate_left_ordered,
           rw ordered at h₁ ⊢,
           cases_matching* (_ ∧ _),
           repeat { split }; try { assumption },
           { apply ihr, assumption, },
-          { apply forall_insert_balanced,
+          { apply forall_insert,
             { exact c₂, },
             { assumption, },
           }
@@ -192,7 +189,7 @@ begin
           cases_matching* (_ ∧ _),
           repeat { split }; try { assumption },
           { apply ihr, assumption, },
-          { apply forall_insert_balanced, 
+          { apply forall_insert, 
             { exact c₂, },
             { assumption, },
           },
@@ -207,18 +204,18 @@ begin
   },
 end
 
-lemma insert_balanced_bound (t : btree α) (k : nat) (v : α) :
-  bound k (insert_balanced k v t) :=
+lemma insert_bound (t : btree α) (k : nat) (v : α) :
+  bound k (insert k v t) :=
 begin
   induction t,
   case empty {
-    simp [insert_balanced, bound],
+    simp [btree.insert, bound],
   },
   case node : tl tk ta tr ihl ihr {
-    simp [insert_balanced],
+    simp [btree.insert],
     by_cases c₁ : (k < tk),
     { simp only [if_pos c₁], 
-      by_cases c₂ : (height tr + 1 < height (insert_balanced k v tl)),
+      by_cases c₂ : ↥(left_heavy (insert k v tl)),
       { simp only [if_pos c₂], 
         apply rotate_right_keys,
         simp [bound],
@@ -232,7 +229,7 @@ begin
     { simp only [if_neg c₁], 
       by_cases c₂ : (tk < k),
       { simp only [if_pos c₂], 
-        by_cases c₃ : (height tl + 1 < height (insert_balanced k v tr)),
+        by_cases c₃ : ↥(right_heavy (insert k v tr)),
         { simp only [if_pos c₃], 
           apply rotate_left_keys,
           simp [bound],
@@ -248,21 +245,21 @@ begin
   },
 end
 
-lemma insert_balanced_diff_bound (t : btree α) (k x : nat) (v : α) :
-  bound x t → bound x (insert_balanced k v t) :=
+lemma insert_diff_bound (t : btree α) (k x : nat) (v : α) :
+  bound x t → bound x (insert k v t) :=
 begin
   intro h₁,
   induction t,
   case empty {
-    simp [insert_balanced, bound],
+    simp [btree.insert, bound],
     simp [bound] at h₁,
     contradiction,
   },
   case node : tl tk ta tr ihl ihr {
-    simp only [insert_balanced],
+    simp only [btree.insert],
     by_cases c₁ : (k < tk),
     { simp only [if_pos c₁], 
-      by_cases c₂ : (height (insert_balanced k v tl) > height tr + 1),
+      by_cases c₂ : ↥(left_heavy (insert k v tl)),
       { simp only [if_pos c₂], 
         apply rotate_right_keys,
         simp [bound],
@@ -278,7 +275,7 @@ begin
     { simp only [if_neg c₁], 
       by_cases c₂ : (k > tk),
       { simp only [if_pos c₂], 
-        by_cases c₃ : (height (insert_balanced k v tr) > height tl + 1),
+        by_cases c₃ : ↥(right_heavy (insert k v tr)),
         { simp only [if_pos c₃], 
           apply rotate_left_keys,
           simp [bound],
@@ -302,20 +299,20 @@ begin
   },
 end
 
-lemma insert_balanced_nbound (t : btree α) (k x : nat) (v : α) :
-  (bound x t = ff ∧ x ≠ k) → bound x (insert_balanced k v t) = ff :=
+lemma insert_nbound (t : btree α) (k x : nat) (v : α) :
+  (bound x t = ff ∧ x ≠ k) → bound x (insert k v t) = ff :=
 begin
   intro h₁,
   induction t,
   case empty {
-    simp [insert_balanced, bound],
+    simp [btree.insert, bound],
     finish,
   },
   case node : tl tk ta tr ihl ihr {
-    simp only [insert_balanced],
+    simp only [btree.insert],
     by_cases c₁ : (k < tk),
     { simp only [if_pos c₁],
-      by_cases c₂ : (height (insert_balanced k v tl) > height tr + 1),
+      by_cases c₂ : ↥(left_heavy (insert k v tl)),
       { simp only [if_pos c₂], 
         apply rotate_right_keys,
         simp [bound] at *,
@@ -329,7 +326,7 @@ begin
     { simp only [if_neg c₁], 
       by_cases c₂ : (tk < k),
       { simp only [if_pos c₂], 
-        by_cases c₃ : (height (insert_balanced k v tr) > height tl + 1),
+        by_cases c₃ : ↥(right_heavy (insert k v tr)),
         { simp only [if_pos c₃],
           apply rotate_left_keys,
           simp [bound] at *,
@@ -350,17 +347,17 @@ end
 
 /- If you check the bound on a key just inserted into the tree, it will return true -/
 lemma bound_insert_eq (k : nat) (t : btree α) (v : α) :
-  bound k (insert_balanced k v t) = tt :=
+  bound k (insert k v t) = tt :=
 begin
   induction t,
   case empty {
-    simp [insert_balanced, bound],
+    simp [btree.insert, bound],
   },
   case node : tl tk ta tr ihl ihr {
-    simp only [insert_balanced],
+    simp only [btree.insert],
     by_cases c₁ : (k < tk),
     { simp only [if_pos c₁], 
-      by_cases c₂ : (height (insert_balanced k v tl) > height tr + 1),
+      by_cases c₂ : ↥(left_heavy (insert k v tl)),
       { simp only [if_pos c₂], 
         apply rotate_right_keys,
         simp [bound],
@@ -373,7 +370,7 @@ begin
     { simp only [if_neg c₁], 
       by_cases c₂ : (k > tk),
       { simp only [if_pos c₂], 
-        by_cases c₃ : (height (insert_balanced k v tr) > height tl + 1),
+        by_cases c₃ : ↥(right_heavy (insert k v tr)),
         { simp only [if_pos c₃], 
           apply rotate_left_keys,
           simp [bound],
@@ -384,6 +381,44 @@ begin
         },
       },
       { simp [if_neg c₂, bound], },
+    },
+  },
+end
+
+lemma insert_balanced (t : btree α) (k : nat) (v : α) :
+  balanced t = tt → balanced (insert k v t) = tt :=
+begin
+  intro h₁,
+  cases t,
+  case empty {
+    simp [btree.insert, balanced],
+  },
+  case node : tl tk ta tr {
+    simp [btree.insert],
+    by_cases c₁ : (k < tk),
+    { simp only [if_pos c₁], 
+      by_cases c₂ : ↥(left_heavy (insert k v tl)),
+      { simp only [if_pos c₂], 
+        apply rotate_right_balanced,
+        sorry,
+      },
+      { simp only [if_neg c₂],
+        simp [balanced] at h₁ ⊢,
+        by_cases c₃ : (height tr ≤ height (insert k v tl)),
+        { simp only [if_pos c₃], 
+          sorry,
+        },
+        { simp only [if_neg c₃], 
+          sorry,
+        },
+      },
+    },
+    { simp only [if_neg c₁], 
+      by_cases c₂ : (tk < k),
+      { simp only [if_pos c₂], 
+        sorry,
+      },
+      { sorry },
     },
   },
 end
